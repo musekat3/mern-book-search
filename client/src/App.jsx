f7dc266b221cf90183f903bc9c5fe86204a0eb41
Kathy Hyde <musekat3@gmail.com>
2023-11-11 17:26:07 -0800

    App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,14 +1,22 @@
 import './App.css';
 import { Outlet } from 'react-router-dom';
+import { ApolloProvider } from '@apollo/client';
+import { ApolloClient, InMemoryCache } from '@apollo/client';
+import Navbar from './components/Navbar';
+
+const client = new ApolloClient({
+  uri: 'http://localhost:3001/graphql', 
+  cache: new InMemoryCache()
+ });
 
 import Navbar from './components/Navbar';
 
 function App() {
   return (
-    <>
+    <ApolloProvider client={client}>
       <Navbar />
       <Outlet />
-    </>
+    </ApolloProvider>
   );
 }
 
