44a1bd281f0df7c8220a7e6a6eb38245eb9c3286
Kathy Hyde <musekat3@gmail.com>
2023-11-11 17:40:06 -0800

    SearchBooks.jsx

--- client/src/pages/SearchBooks.jsx
+++ client/src/pages/SearchBooks.jsx
@@ -9,14 +9,19 @@ import {
 } from 'react-bootstrap';
 
 import Auth from '../utils/auth';
-import { saveBook, searchGoogleBooks } from '../utils/API';
+import { searchGoogleBooks } from '../utils/API';
 import { saveBookIds, getSavedBookIds } from '../utils/localStorage';
+import { SAVE_BOOK } from '../utils/mutations'; // import your SAVE_BOOK mutation
+import { useMutation } from '@apollo/client'; // import the useMutation Hook
 
 const SearchBooks = () => {
   // create state for holding returned google api data
   const [searchedBooks, setSearchedBooks] = useState([]);
   // create state for holding our search field data
   const [searchInput, setSearchInput] = useState('');
+  // call the useMutation Hook with your SAVE_BOOK mutation
+  const [saveBook, { Error }] = useMutation(SAVE_BOOK);
+
 
   // create state to hold saved bookId values
   const [savedBookIds, setSavedBookIds] = useState(getSavedBookIds());
@@ -72,7 +77,7 @@ const SearchBooks = () => {
     }
 
     try {
-      const response = await saveBook(bookToSave, token);
+      const response = await saveBook({ variables: { bookData: bookToSave } });
 
       if (!response.ok) {
         throw new Error('something went wrong!');
